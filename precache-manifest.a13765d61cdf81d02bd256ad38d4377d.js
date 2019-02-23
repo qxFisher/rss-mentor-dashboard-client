9ba5237a73eb5fb0e5e647b8292ba4d087ab4556
Lesnik Matvey <qxfisher@gmail.com>
2019-02-23 20:11:45 +0300

    Updates

---
+++ precache-manifest.a13765d61cdf81d02bd256ad38d4377d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffb56ebec497f1d0da3d",
+    "url": "/rss-mentor-dashboard-client/static/js/runtime~main.ffb56ebe.js"
+  },
+  {
+    "revision": "f5ec04a3e77c5da4492f",
+    "url": "/rss-mentor-dashboard-client/static/js/main.f5ec04a3.chunk.js"
+  },
+  {
+    "revision": "1543426e44c4136549ee",
+    "url": "/rss-mentor-dashboard-client/static/js/2.1543426e.chunk.js"
+  },
+  {
+    "revision": "f5ec04a3e77c5da4492f",
+    "url": "/rss-mentor-dashboard-client/static/css/main.9c95c7ca.chunk.css"
+  },
+  {
+    "revision": "b314c9ba4b4c91d8d2d7928b8e3e5f04",
+    "url": "/rss-mentor-dashboard-client/index.html"
+  }
+];
